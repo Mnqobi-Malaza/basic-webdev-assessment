ac1a3956fdc63c8398e18bbcabb93160881de3b0
Mnqobi-Malaza <38684183+Mnqobi-Malaza@users.noreply.github.com>
2023-02-06 05:12:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 function putTodo(todo) {
     // implement your code here
     let todoId = todo.id;
-    fetch(window.location.href + 'api/todo/todoId',todo)
-    .then(response => response.json())
+    
+    const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ id: todo.id,
+                           title: todo.title,
+                           description: todo.description,
+                           done: todo.done
+                         })
+    };
+    
+    fetch(window.location.href + 'api/todo/todoId',requestOptions)
+    .then(response => {
+         if (!response.ok) {
+            const error =  response.status;
+            throw new Error(error);
+        }
+        showToastMessage('Successfully updated todo......')
+    })
     .catch(error => showToastMessage('Failed to update todo...'));
     
     console.log("calling putTodo");
@@ -11,8 +28,24 @@ function putTodo(todo) {
 
 function postTodo(todo) {
     // implement your code here
-    fetch(window.location.href + 'api/todo',todo)
-    .then(response => response.json())
+    const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ id: todo.id,
+                           title: todo.title,
+                           description: todo.description,
+                           done: todo.done
+                         })
+    };
+    
+    fetch(window.location.href + 'api/todo', requestOptions)
+    .then(response => {
+        if (!response.ok) {
+            const error =  response.status;
+            throw new Error(error);
+        }
+        showToastMessage('Successfully created todo...')
+    })
     .catch(error => showToastMessage('Failed to post todos...'));
     
     console.log("calling postTodo");
@@ -22,8 +55,15 @@ function postTodo(todo) {
 function deleteTodo(todo) {
     // implement your code here
     let todoId = todo.id;
-    fetch(window.location.href + 'api/todo/todoId')
-    .then(response => response.json())
+    
+    fetch(window.location.href + 'api/todo/todoId', { method: 'DELETE' })
+    .then(response => {
+        if (!response.ok) {
+            const error =  response.status;
+            throw new Error(error);
+        }
+        showToastMessage('Successfully deleted todo...')
+    })
     .catch(error => showToastMessage('Failed to delete todo...'));
     
     console.log("calling deleteTodo");
