f2ed4ccd6fb254b1c546383d735876abc71682eb
Mnqobi-Malaza <38684183+Mnqobi-Malaza@users.noreply.github.com>
2023-02-06 04:32:20 +0200

    Update app.js
    added create, post, delete functionality

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 function putTodo(todo) {
     // implement your code here
+    let todoId = todo.id;
+    fetch(window.location.href + 'api/todo/todoId',todo)
+    .then(response => response.json())
+    .catch(error => showToastMessage('Failed to update todo...'));
+    
     console.log("calling putTodo");
     console.log(todo);
 }
 
 function postTodo(todo) {
     // implement your code here
+    fetch(window.location.href + 'api/todo',todo)
+    .then(response => response.json())
+    .catch(error => showToastMessage('Failed to post todos...'));
+    
     console.log("calling postTodo");
     console.log(todo);
 }
 
 function deleteTodo(todo) {
     // implement your code here
+    let todoId = todo.id;
+    fetch(window.location.href + 'api/todo/todoId')
+    .then(response => response.json())
+    .catch(error => showToastMessage('Failed to delete todo...'));
+    
     console.log("calling deleteTodo");
     console.log(todo);
 }
