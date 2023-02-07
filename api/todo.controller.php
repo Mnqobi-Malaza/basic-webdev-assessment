3b86cf31d71e90716b231ee99023eed1773485f7
Mnqobi-Malaza <38684183+Mnqobi-Malaza@users.noreply.github.com>
2023-02-07 03:38:27 +0200

    Update todo.controller.php
    Implementing create, delete, update

--- api/todo.controller.php
+++ api/todo.controller.php
@@ -34,17 +34,60 @@ class TodoController {
 
     public function create(Todo $todo) : bool {
         // implement your code here
-        return true;
+        if(!empty($todo)){ 
+         
+            if(!empty($this->todos[])){ 
+                array_push($this->todos, $todo); 
+            }else{ 
+                $this->todos[] = new Todo($todo->id, $todo->title, $todo->description, $todo->done); 
+            } 
+            
+            $insert = file_put_contents($this->PATH, json_encode($this->todos)); 
+             
+            return $insert?true:false; 
+        }else{ 
+            return false; 
+        } 
+        
     }
 
     public function update(string $id, Todo $todo) : bool {
         // implement your code here
-        return true;
+        if(!empty($todo) && !empty($id)){ 
+             
+            foreach($this->todos as $todoExisting) {
+            if ($todoExisting->id == $id) {
+                $todoExisting->title = $todo->title;
+                $todoExisting->description = $todo->description;
+                $todoExisting->done = $todo->done;    
+                return $todoExisting;
+            }
+                 
+            $update = file_put_contents($this->PATH, json_encode($todos)); 
+             
+            return $update?true:false; 
+        }else{ 
+            return false; 
+        } 
     }
 
     public function delete(string $id) : bool {
         // implement your code here
-        return true;
+        $content = file_get_contents($this->PATH); 
+        
+        if ($content === false) {
+            throw new Exception(self::PATH . " does not exist");
+        }  
+        
+        $dataArray = json_decode($content);
+        
+        $newData = array_filter($dataArray, function ($var) use ($id) { 
+            return ($var['id'] != $id); 
+        }); 
+        
+        $delete = file_put_contents($this->PATH, json_encode($newData)); 
+        
+        return $delete?true:false;
     }
 
     // add any additional functions you need below
