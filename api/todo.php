ce855f734177e82e01bb7e0cedab5ff4e1de6773
Mnqobi-Malaza <38684183+Mnqobi-Malaza@users.noreply.github.com>
2023-02-07 04:56:37 +0200

    Update todo.php
    detecting http requests

--- api/todo.php
+++ api/todo.php
@@ -28,12 +28,51 @@ try {
             break;
         case 'POST':
             //implement your code here
+             if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
+                $id = $path[$pathCount - 1];
+                $todo = $controller->load($id);
+                $isCreated = $controller->create($todo);
+                if ($isCreated) {
+                    http_response_code(201);
+                    die(json_encode($todo));
+                }
+                
+            } else {
+               http_response_code(404);
+               die();
+            }
             break;
         case 'PUT':
             //implement your code here
+             if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
+                $id = $path[$pathCount - 1];
+                $todo = $controller->load($id);
+                $isUpdated = $controller->update($id,$todo);
+                 if ($isUpdated) {
+                    http_response_code(200);
+                    die(json_encode($todo));
+                }
+                
+            } else {
+                http_response_code(404);
+                die();
+            }
             break;
         case 'DELETE':
             //implement your code here
+             if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
+                $id = $path[$pathCount - 1];
+                $todo = $controller->load($id);
+                $isDeleted = $controller->delete($id);
+                 if ($isDeleted) {
+                    http_response_code(200);
+                    die(json_encode($todo));
+                }
+                
+            } else {
+                http_response_code(404);
+                die();
+            }
             break;
         default:
             http_response_code(501);
